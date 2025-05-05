67e654ea2e8e581af56b75d77c2e41fbba4f6d7c
Pablo Garcia Miranda <pablo.garciamiranda@swisscom.com>
2025-05-05 11:34:32 +0200

    Adding comment to clarify that vCluster will be deployed to the NAMESPACE passed as an argument

--- kind/deploy-netbox.sh
+++ kind/deploy-netbox.sh
@@ -14,6 +14,7 @@ fi
 
 CLUSTER=$1
 VERSION=$2
+# The specified namespace will be used for both the NetBox deployment and the vCluster creation
 NAMESPACE=$3
 
 # Treat the optional fourth argument "--vcluster" as a boolean flag
