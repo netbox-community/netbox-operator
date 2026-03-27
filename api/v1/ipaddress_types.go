08fecc5a152a1b7a581c89e2098428fdedfa849b
MaIT-HA <36630214+MaIT-HA@users.noreply.github.com>
2026-03-27 11:06:12 +0100

    Apply suggestion from @bruelea
    Co-authored-by: bruelea <166021996+bruelea@users.noreply.github.com>

--- api/v1/ipaddress_types.go
+++ api/v1/ipaddress_types.go
@@ -74,7 +74,7 @@ type IpAddressStatus struct {
 
 	// Last updated
 	// Format: date-time
-	LastUpdated *metav1.Time `json:"last_updated,omitempty"`
+	LastUpdated *metav1.Time `json:"lastUpdated,omitempty"`
 
 	// The URL to the resource in the NetBox UI. Note that the base of this
 	// URL depends on the runtime config of NetBox Operator
